ab871f6ab1bd1cce264e4415529f9228086b5703
arista <aristakhoiru29@gmail.com>
2020-10-17 12:52:23 +0700

    arista_tugas1

---
+++ app/src/main/java/com/example/a1818023_arista/aris.java
@@ -0,0 +1,14 @@
+package com.example.a1818023_arista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aris);
+    }
+}
